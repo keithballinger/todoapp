e5ea781d3b0308d0530001d1255d9146cedc0e73
Ela Malani <ela.sharda@gmail.com>
2017-01-11 20:35:32 -0800

    Basic todo app

---
+++ app/src/main/java/todoapp/microsoft/com/todoapp/MainActivity.java
@@ -0,0 +1,73 @@
+package todoapp.microsoft.com.todoapp;
+
+import android.content.ContentValues;
+import android.nfc.Tag;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+
+import todoapp.microsoft.com.todoapp.db.TaskContract;
+import todoapp.microsoft.com.todoapp.db.TaskDbHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private todoapp.microsoft.com.todoapp.db.TaskDbHelper mHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHelper = new TaskDbHelper(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                              //  String task = String.valueOf(taskEditText.getText());
+                               // Log.d(TAG, "Task to add: " + task);
+
+                                String task = String.valueOf(taskEditText.getText());
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+               // Log.d(TAG, "Add a new task");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
