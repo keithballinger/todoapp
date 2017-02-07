c18383666684999fc86358b0b985347cafa64b62
Karl <kapiteir@microsoft.com>
2017-02-07 10:02:46 -0800

    Added mobile center

--- app/src/main/java/com/microsoft/todoapp/activities/MainActivity.java
+++ app/src/main/java/com/microsoft/todoapp/activities/MainActivity.java
@@ -9,13 +9,21 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
+import com.microsoft.azure.mobile.ingestion.models.Log;
+import com.microsoft.azure.mobile.utils.MobileCenterLog;
 import com.microsoft.todoapp.R;
 import com.microsoft.todoapp.database.DatabaseHelper;
 import com.microsoft.todoapp.exceptions.InvalidValueException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,12 +35,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MobileCenter.start(getApplication(), "9d7eb0d1-aab3-49c9-b921-562a297230d6",
+                Analytics.class, Crashes.class);
+        MobileCenter.setLogLevel(android.util.Log.VERBOSE);
+
         findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LinearLayout layout = (LinearLayout) getLayoutInflater().inflate(R.layout.new_task, null);
                 final EditText taskEditText = (EditText) layout.findViewById(R.id.task);
                 taskEditText.setSingleLine();
+
+                Analytics.trackEvent("+ clicked");
+                final Spinner typeSpinner = (Spinner) layout.findViewById(R.id.type);
+
                 AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
                         .setTitle(R.string.add_task_dialog_title)
                         .setView(layout)
@@ -40,12 +56,17 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 saveTask(String.valueOf(taskEditText.getText()));
+
+                                Map<String,String> properties=new HashMap<String,String>();
+                                properties.put("Task Type", typeSpinner.getSelectedItem().toString());
+                                Analytics.trackEvent("Task Added", properties);
                             }
                         })
                         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 // Call SDK for event
+                                Analytics.trackEvent("Task Cancelled");
                             }
                         })
                         .create();
